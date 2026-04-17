44e4e12c7e20724441ce74a638585abdbd4c790b
Syed <Syed@SYED-PC-001>
2026-04-17 15:22:06 +0400

    Project Structure Developed as Four Tier Architecture

---
+++ Course.Business/BusinessRegistration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Business
+{
+    public class BusinessRegistration
+    {
+    }
+}
---
+++ Course.Infrastructure/InfraRegistration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Infrastructure
+{
+    public class InfraRegistration
+    {
+    }
+}
